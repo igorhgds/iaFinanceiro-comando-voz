b6a0ac16d8acffb26782b52faa556851d6f99d3d
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-04-24 17:53:57 -0300

    subindo projeto base iniciado com spring initializr

---
+++ src/main/java/igor/henrique/iaFinanceiro/IaFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package igor.henrique.iaFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/igor/henrique/iaFinanceiro/IaFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.henrique.iaFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
