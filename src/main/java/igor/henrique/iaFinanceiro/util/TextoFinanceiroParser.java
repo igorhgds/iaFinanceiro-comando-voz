31d2cab66afb14e3e0deeee579d1f4ffa7e63687
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-28 13:02:18 -0300

    refactor: remoção de classes obsoletas

--- src/main/java/igor/henrique/iaFinanceiro/util/TextoFinanceiroParser.java
+++
@@ -1,9 +0,0 @@
-package igor.henrique.iaFinanceiro.util;
-
-// Essa classe era responsável por parsing manual de tipo e mês.
-// Atualmente, o projeto utiliza a IA (ExtratorComInstruct) para essas interpretações.
-// Portanto, esta classe está obsoleta e pode ser removida futuramente.
-
-public class TextoFinanceiroParser {
-    // Classe atualmente obsoleta.
-}
